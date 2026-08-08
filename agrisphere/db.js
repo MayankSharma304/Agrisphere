e91f2bcf8a38d8712f31ec3e65b2d162e635f477
MayankSharma304 <mayankbhai2988@gmail.com>
2026-08-08 12:22:14 +0530

    Delete agrisphere/db.js

--- agrisphere/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-console.log("javaScript Connected!");
-const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/agrisphere");
-  console.log("MongoDB Connected");
-};
-
-module.exports = connectDB;
