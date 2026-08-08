0d6302d025bf20d1dea61cfb52efe6e096e46ffe
MayankSharma304 <mayankbhai2988@gmail.com>
2026-08-08 12:20:52 +0530

    Delete agrisphere/Fertilizer.js

--- agrisphere/Fertilizer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-console.log("javaScript Connected!");
-const fertilizerSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  image: String
-});
-
-module.exports = mongoose.model('Fertilizer', fertilizerSchema);
